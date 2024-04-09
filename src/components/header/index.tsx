1941eac9dc8e9672a807c44ea0c7f0a843ccc075
Claudio Lins <claudio.lins@me.com>
2024-04-09 14:36:13 +0100

    feat: create Header Mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,43 +1,25 @@
-import { ChevronDown, Search, ShoppingCart, User } from 'lucide-react'
+import { Search, ShoppingCart, User } from 'lucide-react'
 import Link from 'next/link'
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { CustonDropdownMenu } from './custon-dropdown-menu'
+import { MenuMobile } from './menu-mobile'
 
 export function Header() {
   return (
-    <div className="fixed inset-0 flex h-16 w-full items-center justify-center bg-white md:h-24">
-      <div className="flex w-full max-w-7xl items-center justify-between space-x-10">
-        <h1 className="font-integralCF cursor-pointer text-[32px] font-black">
+    <div className="fixed flex h-16 w-full items-center justify-center bg-white px-4 md:h-24">
+      <MenuMobile />
+      <div className="ml-2 flex w-full max-w-7xl items-center justify-between space-x-10">
+        <h1 className="font-integralCF -mt-1 cursor-pointer text-2xl font-bold md:text-[32px]">
           SHOP.CO
         </h1>
-        <div className="font-satoshi flex flex-1 items-center space-x-4">
-          <DropdownMenu>
-            <DropdownMenuTrigger className="font-satoshi flex items-end space-x-0 focus:outline-none">
-              <span>Shop</span>
-              <ChevronDown className="h-4 w-4" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+        <div className="font-satoshi hidden flex-1 items-center space-x-4 md:flex">
+          <CustonDropdownMenu />
 
           <Link href={'#'}>On Sales</Link>
           <Link href={'#'}>New Arrivals</Link>
           <Link href={'#'}>Brands</Link>
         </div>
-        <div className="flex h-12 w-[577px] items-center rounded-full bg-gray-200 px-4">
+        <div className="hidden h-12 w-[577px] items-center rounded-full bg-gray-200 px-4 md:flex">
           <Search className="text-gray-400" />
           <input
             type="text"
@@ -46,8 +28,9 @@ export function Header() {
           />
         </div>
         <div className="flex items-center space-x-4">
-          <ShoppingCart className="h-6 w-6 cursor-pointer" />
-          <User className="h-6 w-6 cursor-pointer" />
+          <Search className="h-5 w-5 cursor-pointer md:hidden" />
+          <ShoppingCart className="h-5 w-5 cursor-pointer" />
+          <User className="h-5 w-5 cursor-pointer" />
         </div>
       </div>
     </div>
